7760b1c7109d45264d53329fa99460bdb0f014e2
Konigdave <davidayenioluremi@gmail.com>
2026-05-31 16:19:20 +0000

    feat: add rds tagging permissions to cd user

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -178,6 +178,8 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
+      "rds:AddTagsToResource",
+      "rds:RemoveTagsFromResource",
       "rds:ModifyDBInstance"
     ]
     resources = ["*"]
