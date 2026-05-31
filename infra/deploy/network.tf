43075f9bca03dfb021f3e897cca69b8270a4423d
Konigdave <davidayenioluremi@gmail.com>
2026-05-31 13:16:00 +0000

    added network

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -20,12 +20,12 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-##################################################
+######
 # Public subnets for load balancer public access #
-##################################################
+######
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.1.1.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${data.aws_region.current.name}a"
 
@@ -55,7 +55,7 @@ resource "aws_route" "public_internet_access_a" {
 
 resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.1.2.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${data.aws_region.current.name}b"
 
