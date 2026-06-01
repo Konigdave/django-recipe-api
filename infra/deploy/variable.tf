89b0b42553427a093eba69a87952663ae8891602
Konigdave <davidayenioluremi@gmail.com>
2026-06-01 22:24:26 +0000

    added container definition for app

--- infra/deploy/variable.tf
+++ infra/deploy/variable.tf
@@ -31,3 +31,12 @@ variable "ecr_proxy_image" {
   description = "The path of the proxy image in ECR."
   type        = string
 }
+
+variable "ecr_app_image" {
+  description = "The path of the app image in ECR."
+  type        = string
+}
+
+variable "django_secret_key" {
+  description = "secret key for django"
+}
