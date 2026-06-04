018c71226a428fb94ea1b72ed03c142a0d86b72e
Konigdave <davidayenioluremi@gmail.com>
2026-06-04 23:36:37 +0000

    fix: add missing protocol to ecs security group

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -176,6 +176,7 @@ resource "aws_security_group" "ecs_service" {
   ingress {
     from_port   = 8000
     to_port     = 8000
+    protocol    = "tcp"
     security_groups = [
       aws_security_group.lb.id
     ]
