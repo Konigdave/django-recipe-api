fa546b0874f238ceefe9eabbfe1dd3b2fdcf6b89
Konigdave <davidayenioluremi@gmail.com>
2026-06-06 22:26:13 +0000

    fix: media path for static files

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -122,7 +122,7 @@ resource "aws_ecs_task_definition" "api" {
       mountPoints = [
         {
           readOnly      = true
-          containerPath = "/vol/web"
+          containerPath = "/vol/web/static"
           sourceVolume  = "static"
         }
       ]
