3de9c3beaba3269b1d04c3d5b4df1c6651ca443d
Konigdave <davidayenioluremi@gmail.com>
2026-06-07 22:19:00 +0000

    updated cloufare value to content

--- infra/deploy/dns.tf
+++ infra/deploy/dns.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "api" {
   zone_id = var.cloudflare_zone_id
   name    = "@"                     # Root domain (david-cloud.site)
-  value   = aws_lb.api.dns_name    # Dynamically tracking your live ALB address!
+  content = aws_lb.api.dns_name    # Dynamically tracking your live ALB address!
   type    = "CNAME"
   proxied = false                   # DNS Only (Gray Cloud)
 }
